1dd05dbe48fdb156e8bac5a2b5338d1fec30f37a
Nicolas <nicolasmilloco@hotmail.com>
2022-05-10 11:58:56 +0200

    condition description/list

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Collapse = ({title, description}) => {
+const Collapse = ({title, description, equipments}) => {
     /* Crée un Hook d'état */
     const [open, setOpen] = useState(false);
 
@@ -9,11 +9,20 @@ const Collapse = ({title, description}) => {
             <div className="dropdown_header" onClick={() => setOpen(!open)}>
                 <div className="dropdown_title">{title}</div>
             </div>
+             {open && 
             <div className="">
-            {open && <div className="dropdown_description">{description}</div>}
+                {description ? <p className="dropdown_description">{description}</p> : <ul className="dropdown_list">{equipments}</ul>} 
             </div>
+            }
         </div>
     );
 }
 
 export default Collapse;
+
+
+    // if (open) {
+    //     return stars.push(<img src={starFull} key={i} alt=""/>)
+    //   } else {
+    //     stars.push(<img src={starEmpty} key={i} alt="" />)
+    //   }
