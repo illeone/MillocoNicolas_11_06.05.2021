9fb385cc8894938970c513c36b8a2c66b93347e4
Nicolas <nicolasmilloco@hotmail.com>
2022-05-16 18:55:34 +0200

    ajout flèches dans gallery avec conditions

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,22 +1,44 @@
 import React, {useState} from "react";
-
+import {ReactComponent as LeftArrow} from '../assets/left.svg';
+import {ReactComponent as RightArrow} from '../assets/right.svg';
 
 
 const Gallery = ({photos}) => {
-    const [currentImage] = useState(0)
+    const [currentImage, setCurrentImage] = useState(0)
+    const length = photos?.length
+    // console.log(length)
 
-    console.log(currentImage)
+    const previousImg = () => {
+      if (currentImage === 0) {
+        setCurrentImage(length - 1)
+      } else {
+        setCurrentImage(currentImage - 1)
+      }
+    }
+    const nextImg = () => {
+      if (currentImage === length - 1) {
+        setCurrentImage(0)
+      } else {
+        setCurrentImage(currentImage + 1)
+      }
+    }
+    // console.log(currentImage)
   
     return (
-      <section className="">
+      <section className="gallery">
 
         {photos?.map((photo, index) => {
           return (
             <div key={index} className="">         
-              {index === currentImage && (<img src={photo} alt="" className="" />)}
+              {index === currentImage && (<img src={photo} alt="" className="gallery_image" />)}
             </div>
+            
             )
             })}
+            <div>
+                <LeftArrow onClick={previousImg} className="left_arrow" style={{background:"blue"}} />
+                <RightArrow onClick={nextImg} className="right_arrow" style={{background:"blue"}} />
+            </div>
         
       </section>
     )
